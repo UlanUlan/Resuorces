33eee1ab9eb5fbcb7888c6399ef9640c5719ae62
ЖолдасхановУ <@223_14>
2018-06-18 21:58:00 +0600

    Add project files.

---
+++ WpfApplication4/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeTemplate(object sender, RoutedEventArgs e)
+        {
+            ResourceDictionary template = new ResourceDictionary();
+
+            RadioButton rb = (RadioButton)sender;
+
+            switch (rb.Name)
+            {
+                case "Dark":
+                    {
+                        template.Source = 
+                            new Uri("Resources/DarkStyle.xaml", UriKind.Relative);
+                    }
+                    break;
+                case "Light":
+                    {
+
+                    }
+                    break;
+            }
+
+            //var currentResource = this.Resources.MergedDictionaries;
+            this.Resources.MergedDictionaries[0] = template;
+        }
+    }
+}
